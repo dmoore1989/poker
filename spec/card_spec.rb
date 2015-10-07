f7af35301ec4fcb4161b4390d6d0974fd0706db4
JimmyDoug <JimmyDoug@email>
2015-10-07 14:34:19 -0400

    Initialize card class

---
+++ spec/card_spec.rb
@@ -0,0 +1,33 @@
+require 'card'
+
+describe Card do
+  let(:card) {Card.new(:k, :H)}
+  describe '#value' do
+
+    it 'is given on creation' do
+      expect(card.value).to eq(:k)
+
+    end
+
+    it 'cannot be changed by the user' do
+      expect{card.value = :q}.to raise_error
+    end
+
+  end
+
+  describe '#suit' do
+
+    it 'is given on creation' do
+      expect(card.suit).to eq(:H)
+
+    end
+
+    it 'cannot be changed by the user' do
+      expect{card.suit = :C}.to raise_error
+    end
+
+
+  end
+
+
+end
